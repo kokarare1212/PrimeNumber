95ec0e5d072d56235ca7eccef4826d016204263e
こうから <kokarare1212@gmail.com>
2020-07-11 08:55:55 +0900

    Add files via upload

---
+++ main.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace PrimeNumberCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 2;i <= 10000000; i++)
+            {
+                int k = (int)Math.Sqrt(i);
+                bool f = true;
+                for(int j = 2; j <= k; j++)
+                {
+                    if(i % j == 0)
+                    {
+                        f = false;
+                    }
+                }
+                if (f)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
