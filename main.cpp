94f176ed8d5ecd6170d364d68708688bf2d24c64
こうから <kokarare1212@gmail.com>
2020-07-12 11:03:36 +0900

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main() {
+    for(int i = 2; i <= 10000000; i++){
+        int k = (int)sqrt(i);
+        bool f = true;
+        for(int j = 2; j <= k; j++){
+            if(i % j == 0){
+                f = false;
+            }
+        }
+        if(f){
+            std::cout << i << std::endl;
+        }
+    }
+    //std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
